38cfeeb05b6343af84140b6dfd6437fe3f5a75c7
CEEJ987 <123979158+CEEJ987@users.noreply.github.com>
2023-05-19 11:10:12 -0500

    intialized origin Mybranch

--- src/App.js
+++ src/App.js
@@ -1,58 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import axios from "axios";
 import React, { useState } from 'react';
-import bootstrap from 'react';
-import Map from './mapcomp'
-
+import Map from './mapcomp';
+import Weather from './weather';
 
 
 function App() {
-  const [displaycityinfo, setDisplaycityinfo] = useState("")
-  
+  const [displaycityinfo, setDisplaycityinfo] = useState({});
+  const [weatherdata, setWeatherdata] = useState([]);
   const [inputcity, setInputcity] = useState("");
-  function locationfinder(event){
-    setInputcity(event.target.value)
-
+  
+  function locationfinder(event) {
+    setInputcity(event.target.value);
+  }
+  
+  async function weatherForecast(lat, lon) {
+    lat = Math.floor(lat * 100) / 100;
+    lon = Math.floor(lon * 100) / 100;
+    try {
+      const weatherforecast = await axios.get(`http://localhost:3001/weather?lat=${lat}&lon=${lon}`);
+      console.log(weatherforecast);
+      setWeatherdata(weatherforecast.data);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  const button = async () => {
-    const apiinfo = await axios.get(`https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONQI_APT_KEY}&q=${inputcity}&format=json`); 
-    console.log(apiinfo)
-    setDisplaycityinfo(apiinfo.data[0])
-    console.log(apiinfo)
-
-    return apiinfo
+  async function button() {
+    try {
+      const apiinfo = await axios.get(`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONQI_APT_KEY}&center=${inputcity}`);
+      console.log(apiinfo);
+      setDisplaycityinfo(apiinfo.data[0]);
+      weatherForecast(apiinfo.data[0].lat, apiinfo.data[0].lon);
+    } catch (error) {
+      console.log(error);
+    }
   }
+  
   return (
     <div className="App">
       <header className="App-header">
-      
-
-    <Form.Control onChange={locationfinder} type="text" placeholder="Enter Location" />
-    <Button onClick={button}>Explore</Button>
-    {displaycityinfo.display_name}
-    {displaycityinfo.lon}
-    {displaycityinfo.lat}
-    <Map lat = {displaycityinfo.lat} lon = {displaycityinfo.lon}/>
-
-  
-
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Form.Control onChange={locationfinder} type="text" placeholder="Enter Location" />
+        <Button onClick={button}>Explore</Button>
+        {displaycityinfo.display_name}
+        {displaycityinfo.lon}
+        {displaycityinfo.lat}
+        <Map lat={displaycityinfo.lat} lon={displaycityinfo.lon} />
+        <Weather forecast={weatherdata} />
       </header>
     </div>
   );
---
+++ src/weather.js
@@ -0,0 +1,11 @@
+function Weather (props){
+    console.log('weather data is ', props.forecast)
+//    let element =
+    
+    return (props.forecast.map((weatherdata) => {
+        return <h1 style={{color:"white"}}>{weatherdata.date}{weatherdata.description}</h1>
+    })
+    );
+}
+
+export default Weather;
