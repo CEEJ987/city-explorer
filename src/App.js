dcfe56f50e8c8e6b7e89c12f48a3141028f97c31
CEEJ987 <123979158+CEEJ987@users.noreply.github.com>
2023-05-19 12:44:50 -0500

    intialized origin Mybranch

--- src/App.js
+++ src/App.js
@@ -3,45 +3,52 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import axios from "axios";
 import React, { useState } from 'react';
+import { bootstrap } from 'react-bootstrap';
 import Map from './mapcomp';
-import Weather from './weather';
+import Weather from './weather'
 
 
 function App() {
-  const [displaycityinfo, setDisplaycityinfo] = useState({});
+  const [displaycityinfo, setDisplaycityinfo] = useState("");
   const [weatherdata, setWeatherdata] = useState([]);
   const [inputcity, setInputcity] = useState("");
-  
+
   function locationfinder(event) {
-    setInputcity(event.target.value);
+    setInputcity(event.target.value)
+
   }
-  
   async function weatherForecast(lat, lon) {
-    lat = Math.floor(lat * 100) / 100;
-    lon = Math.floor(lon * 100) / 100;
-    try {
-      const weatherforecast = await axios.get(`http://localhost:3001/weather?lat=${lat}&lon=${lon}`);
-      console.log(weatherforecast);
-      setWeatherdata(weatherforecast.data);
-    } catch (error) {
-      console.log(error);
-    }
+    lat = Math.floor(lat * 100) / 100
+    lon = Math.floor(lon * 100) / 100
+    const weatherinfo = await axios.get(`http://localhost:3001/weather?lat=${lat}&lon=${lon}`)
+      .then((weatherforecast) => {
+        console.log(weatherforecast)
+        setWeatherdata(weatherforecast.data)
+      })
+    console.log(weatherinfo)
   }
 
-  async function button() {
-    try {
-      const apiinfo = await axios.get(`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONQI_APT_KEY}&center=${inputcity}`);
-      console.log(apiinfo);
-      setDisplaycityinfo(apiinfo.data[0]);
-      weatherForecast(apiinfo.data[0].lat, apiinfo.data[0].lon);
-    } catch (error) {
-      console.log(error);
-    }
+  const button = async () => {
+    const apiinfo = await axios.get(`https://us1.locationiq.com/v1/search?key=pk.4c85523dcc468cdc9fc82156184b6529&q=${inputcity}&format=json`)
+      .then(async function (response) {
+        console.log(response)
+        setDisplaycityinfo(response.data[0])
+        weatherForecast(response.data[0].lat, response.data[0].lon);
+
+      })
+    // console.log(apiinfo)
+    // setDisplaycityinfo(apiinfo.data[0])
+    // console.log(apiinfo)
+
+    return apiinfo
   }
-  
+  const imageholder = <img src={`https://maps.locationiq.com/v3/staticmap?key=pk.4c85523dcc468cdc9fc82156184b6529&center=${displaycityinfo.lat},${displaycityinfo.lon}&zoom=12`} />
+
   return (
     <div className="App">
       <header className="App-header">
+
+
         <Form.Control onChange={locationfinder} type="text" placeholder="Enter Location" />
         <Button onClick={button}>Explore</Button>
         {displaycityinfo.display_name}
@@ -49,6 +56,10 @@ function App() {
         {displaycityinfo.lat}
         <Map lat={displaycityinfo.lat} lon={displaycityinfo.lon} />
         <Weather forecast={weatherdata} />
+
+        {imageholder}
+
+
       </header>
     </div>
   );
